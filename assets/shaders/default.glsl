5188672aea608aca2cc7ff8229da3f5fa59e0f2c
Drago <parthchokhda@gmail.com>
2023-11-11 11:40:49 +0530

    .

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -35,32 +35,30 @@ void main(){
         int id = int(fTexID);
         switch (id) {
             case 0:
-            color = fColor * texture(uTextures[0], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[0], fTexCoords);
+                break;
             case 1:
-            color = fColor * texture(uTextures[1], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[1], fTexCoords);
+                break;
             case 2:
-            color = fColor * texture(uTextures[2], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[2], fTexCoords);
+                break;
             case 3:
-            color = fColor * texture(uTextures[3], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[3], fTexCoords);
+                break;
             case 4:
-            color = fColor * texture(uTextures[4], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[4], fTexCoords);
+                break;
             case 5:
-            color = fColor * texture(uTextures[5], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[5], fTexCoords);
+                break;
             case 6:
-            color = fColor * texture(uTextures[6], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[6], fTexCoords);
+                break;
             case 7:
-            color = fColor * texture(uTextures[7], fTexCoords);
-            break;
+                color = fColor * texture(uTextures[7], fTexCoords);
+                break;
         }
-//        color = fColor * texture(uTextures[id], fTexCoords);
-//        color = fColor * texture(uTextures, vec3(fTexCoords.xy, id));
     }else {
         color  = fColor;
     }
--- src/main/java/engine/LevelEditorScene.java
+++ src/main/java/engine/LevelEditorScene.java
@@ -19,25 +19,6 @@ public class LevelEditorScene extends Scene {
 
         Spritesheet sprites = AssetPool.getSpritesheet("assets/images/spritesheet.png");
 
-//        int xOffset = 10;
-//        int yOffset = 10;
-//        float totalWidth = (float)(600 - xOffset * 2);
-//        float totalHeight = (float)(300 - yOffset * 2);
-//
-//        float sizex = totalWidth / 100.0f;
-//        float sizey = totalHeight / 100.0f;
-//
-//        for(int x = 0; x < 100 ; x++) {
-//            for(int y = 0; y < 100 ;  y++) {
-//                float xPos = xOffset + (x * sizex);
-//                float yPos = yOffset + (y * sizey);
-//
-//                GameObject go = new GameObject("OBJECT" + x + " " + y, new Transform(new Vector2f(xPos,yPos) , new Vector2f(sizex,sizey)));
-//                go.addComponent(new SpriteRenderer(new Vector4f(xPos / totalWidth , yPos / totalHeight,1,1)));
-//                this.addGameObjectToScene(go);
-//            }
-//        }
-
         GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
