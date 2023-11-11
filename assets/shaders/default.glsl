af6704f7e30a9f67fc01252d44ae7b563cfb1cd7
Drago <parthchokhda@gmail.com>
2023-11-11 11:36:17 +0530

    Image issue resolved

--- assets/shaders/default.glsl
+++ assets/shaders/default.glsl
@@ -26,15 +26,41 @@ in vec4 fColor;
 in vec2 fTexCoords;
 in float fTexID;
 
-uniform sampler2DArray uTextures;
+uniform sampler2D uTextures[8];
 
 out vec4 color;
 
 void main(){
     if (fTexID > 0){
         int id = int(fTexID);
+        switch (id) {
+            case 0:
+            color = fColor * texture(uTextures[0], fTexCoords);
+            break;
+            case 1:
+            color = fColor * texture(uTextures[1], fTexCoords);
+            break;
+            case 2:
+            color = fColor * texture(uTextures[2], fTexCoords);
+            break;
+            case 3:
+            color = fColor * texture(uTextures[3], fTexCoords);
+            break;
+            case 4:
+            color = fColor * texture(uTextures[4], fTexCoords);
+            break;
+            case 5:
+            color = fColor * texture(uTextures[5], fTexCoords);
+            break;
+            case 6:
+            color = fColor * texture(uTextures[6], fTexCoords);
+            break;
+            case 7:
+            color = fColor * texture(uTextures[7], fTexCoords);
+            break;
+        }
 //        color = fColor * texture(uTextures[id], fTexCoords);
-        color = fColor * (texture(uTextures, vec3(fTexCoords.xy, id)));
+//        color = fColor * texture(uTextures, vec3(fTexCoords.xy, id));
     }else {
         color  = fColor;
     }
